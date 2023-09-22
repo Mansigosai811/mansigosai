6f163de84d792d393ab0f29adc6367a9bb560be1
Mansigosai811 <137849969+Mansigosai811@users.noreply.github.com>
2023-09-22 16:10:39 +0530

    Add files via upload

---
+++ multipliction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,multipliction;
+	printf("enter the nymber:");
+	scanf("%d",&a);
+	printf("enter the secound number:");
+	scanf("%d",&b);
+	multipliction=a*b;
+	printf("%d * %d = %d",a,b,multipliction);
+	
+}
