0968b692fc44db7093724c75319cb445ce52272d
Mansigosai811 <gosaimansi811@gmail.com>
2023-09-22 16:08:10 +0530

    Done
    Done

---
+++ subtraction.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,subtraction;
+	printf("entre the number:");
+	scanf("%d",&a);
+	printf("enter the secound number:");
+	scanf("%d",&b);
+	subtraction=a-b;
+	printf("%d - %d = %d",a,b,subtraction);
+}
